81f71b06a854e0f3624c9be1ac5dec60e4a6d108
jiafeixiaoye <473569528@qq.com>
2018-01-11 11:30:02 +0800

    Delete cathy_train.sh

--- tools/cathy_train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tools/train_net.py --gpu 0 --solver models/pvanet/example_train/solver.prototxt --weights models/pvanet/pretrained/pvanet_frcnn_iter_1350000.caffemodel --iters 500000 --cfg models/pvanet/cfgs/train.yml --imdb voc_2007_trainval
